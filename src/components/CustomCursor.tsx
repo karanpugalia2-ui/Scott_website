008a877d5dc88ddce061d9ad4f9fa7d9ba7e7efc
ishaan-bits <ishaan.parimal49@gmail.com>
2026-09-13 18:48:11 +0530

    Cursor ring lerp 0.75 -> 0.85

--- src/components/CustomCursor.tsx
+++ src/components/CustomCursor.tsx
@@ -26,8 +26,8 @@ export default function CustomCursor() {
     };
 
     const animate = () => {
-      cursorX += (mouseX - cursorX) * 0.75;
-      cursorY += (mouseY - cursorY) * 0.75;
+      cursorX += (mouseX - cursorX) * 0.85;
+      cursorY += (mouseY - cursorY) * 0.85;
       cursor.style.transform = `translate(${cursorX - 20}px, ${cursorY - 20}px)`;
       rafRef.current = requestAnimationFrame(animate);
     };
