629d8ace24c2ec7ab2c05ae964c864bbf18781d7
ishaan-bits <ishaan.parimal49@gmail.com>
2026-09-22 18:01:31 +0530

    Widen gallery to max-w-5xl to reduce side gaps

--- src/components/ArtistGallery.tsx
+++ src/components/ArtistGallery.tsx
@@ -44,7 +44,7 @@ export default function ArtistGallery({
           onClick={onClose}
         >
           <motion.div
-            className="w-full max-w-4xl flex flex-col min-h-0 flex-1"
+            className="w-full max-w-5xl flex flex-col min-h-0 flex-1"
             initial={{ opacity: 0, y: 24 }}
             animate={{ opacity: 1, y: 0 }}
             exit={{ opacity: 0, y: 24 }}
